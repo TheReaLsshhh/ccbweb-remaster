8ed15ad4ec6848f11f952552da8025c4064046ee
John Kenneth Salgado <salgadojohnkenneth98@gmail.com>
2025-08-28 16:02:55 +0800

    Initial Commit

--- ccb_portal_backend/settings.py
+++ ccb_portal_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6epnt%znb*)x+vj1*&+r)f33dr(%i)bkdy(gc9fk@h%#+3c@3)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
